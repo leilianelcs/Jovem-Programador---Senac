157d72ca102cb26b1f0e6f43628e3379d5ef3212
leilianelcs <leilianelcs@gmail.com>
2025-08-19 19:27:06 -0300

    Adicionando exercícios aula 19-08

---
+++ veiculo/src/main/java/com/veiculo/mapper/FabricanteMapper.java
@@ -0,0 +1,27 @@
+package com.veiculo.mapper;
+
+import com.veiculo.dto.FabricanteDTO;
+import com.veiculo.entity.Fabricante;
+
+public final class FabricanteMapper {
+
+    private FabricanteMapper() {
+    }
+
+    public static FabricanteDTO toDTO(Fabricante entity) {
+        if (entity == null)
+            return null;
+        return new FabricanteDTO(entity.getId(), entity.getNome(), entity.getPaisOrigem());
+    }
+
+    public static Fabricante toEntity(FabricanteDTO dto) {
+        if (dto == null)
+            return null;
+        Fabricante f = new Fabricante();
+        f.setId(dto.getId());
+        f.setNome(dto.getNome());
+        f.setPaisOrigem(dto.getPaisOrigem());
+        return f;
+
+    }
+}
--- veiculo/src/main/java/com/veiculo/repository/FabricanteRepository.java
+++ veiculo/src/main/java/com/veiculo/repository/FabricanteRepository.java
@@ -1,6 +1,12 @@
 package com.veiculo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.veiculo.entity.Fabricante;
+
 @Repository
-public interface FabricanteRepository extends
+public interface FabricanteRepository extends JpaRepository<Fabricante, Long> {
+    boolean existsByNome(String nome); // validar no banco para verificar se é possível inserir o dado (um nome ou cpf
+                                       // com unique, por exemplo, que não pode cadastrar igual)
+}
