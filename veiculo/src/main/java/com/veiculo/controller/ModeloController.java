2ba250c7b719ad2d0667370310bd27d8d02b2480
leilianelcs <leilianelcs@gmail.com>
2025-09-01 21:56:03 -0300

    Atualizando exercício

--- veiculo/src/main/java/com/veiculo/controller/ModeloController.java
+++ veiculo/src/main/java/com/veiculo/controller/ModeloController.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.veiculo.dto.FabricanteDTO;
 import com.veiculo.dto.ModeloDTO;
 import com.veiculo.service.ModeloService;
 
@@ -19,6 +23,11 @@ public class ModeloController {
 
     @Autowired
     private ModeloService service;
+
+    @GetMapping("/{id}")
+    public ModeloDTO buscar(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
 
     @GetMapping
     public List<ModeloDTO> listar() {
@@ -31,6 +40,17 @@ public class ModeloController {
 
         return ResponseEntity.created(null).body(criado);
 
+    }
+
+    @PutMapping("/{id}")
+    public ModeloDTO atualizar(@PathVariable Long id, @RequestBody ModeloDTO dto) {
+        return service.atualizar(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
